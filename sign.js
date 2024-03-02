0435f023c41f06b9d2a524f42c2b2eb640b798e8
rachitkansal05 <iit2023100@iiita.ac.in>
2024-03-02 17:08:35 +0530

    kahjkdbf

--- sign.js
+++ sign.js
@@ -2,6 +2,7 @@ const convertedText = document.getElementById('converted_text');
 const startButton = document.getElementById('click_to_record');
 const submitButton = document.getElementById('submit_text');
 const videoDisplay = document.getElementById('video_display');
+const videoContainer = document.getElementById('video_container'); // Define videoContainer
 let recognition;
 
 if ('webkitSpeechRecognition' in window) {
@@ -26,9 +27,9 @@ function toggleRecording() {
     }
 }
 
-let lastTranscript = ''; 
+let lastTranscript = '';
 startButton.addEventListener('click', () => {
-    recognition.start(); 
+    recognition.start();
     startButton.textContent = 'Recording...';
 });
 
@@ -36,7 +37,7 @@ recognition.onresult = (event) => {
     const result = event.results[event.results.length - 1];
     const transcript = result[0].transcript;
     convertedText.value += transcript;
-    lastTranscript = transcript; 
+    lastTranscript = transcript;
 };
 
 recognition.onend = () => {
@@ -47,24 +48,25 @@ recognition.onerror = (event) => {
     console.error('Speech recognition error detected: ' + event.error);
 };
 
-submitButton.addEventListener('click', () => { 
-    handleTranscript(lastTranscript); 
+submitButton.addEventListener('click', () => {
+    handleTranscript(lastTranscript);
     convertedText.value = '';
+    videoContainer.style.display = 'block'; // Change display to "block"
 });
-startButton.addEventListener('click', () => { 
+startButton.addEventListener('click', () => {
     convertedText.value = '';
 });
 
 function handleTranscript(transcript) {
     let letter = transcript.trim().toUpperCase();
     let videoPath = letter + ".mp4";
-    console.log("Video Path:", videoPath); 
+    console.log("Video Path:", videoPath);
     // Pause the video before changing the source
     videoDisplay.pause();
     videoDisplay.src = videoPath;
     videoDisplay.alt = 'my video';
     videoDisplay.width = 200;
-    videoDisplay.height = 400; 
+    videoDisplay.height = 400;
     // Reset the video to start from the beginning
     videoDisplay.currentTime = 0;
     // Start playing the video
